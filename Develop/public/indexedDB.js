142660e089d2e90f9cdfd1e66595aa7a0951a152
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-01 12:53:02 -0500

    checking the dataBase and start the transaction,have access to pending records

--- Develop/public/indexedDB.js
+++ Develop/public/indexedDB.js
@@ -31,7 +31,7 @@ const indexedDB =
     request.onerror = function (event) {
         console.log("Error Code")
     };
-//using the fuction saveRecorde to save the data,access and record
+// using the funtion saveRecord to save, access and record the data
     function saveRecord(data) {
         const transaction = db.transaction(["pending"], "readwrite");
         //access
@@ -39,4 +39,34 @@ const indexedDB =
         //add record
         store.add(data);
     }
+    //checking the database to help start the transaction,
+    function checkDatabase() {
+        //start transaction
+        const transaction = db.transaction(["pending"], "readwrite");
+        //access pending records
+        const store = transaction.objectStore("pending");
+        //getting all the record from the store
+        const getAll = store.getAll();
     
+        getAll.onsuccess = function () {
+            if (getAll.result.length > 0) {
+                fetch("/api/transaction/bulk", {
+                        method: "POST",
+                        body: JSON.stringify(getAll.result),
+                        headers: {
+                            Accept: "application/json, text/plain, */*",
+                            "Content-Type": "application/json"
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(() => {
+                        const transaction = db.transaction(["pending"], "readwrite");
+                        const store = transaction.objectStore("pending");
+                        store.clear();
+                    });
+            }
+        };
+    
+    };
+    
+    window.addEventListener("online", checkDatabase);
