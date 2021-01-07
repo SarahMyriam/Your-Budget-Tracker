7fadba20a31a4304e17066f67a6bbe598f37da27
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2021-01-06 22:24:57 -0500

    fixed my db file and service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener("install", function (evt) {
 
     // pre cache all static assets
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)).catch(err  => console.log(err))
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)).catch(err => console.log(err))
     );
 
     // tell the browser to activate this service worker immediately once it
@@ -43,37 +43,40 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
-// fetch
+// fetch for offline /online problem
 self.addEventListener("fetch", function (evt) {
-    console.log("fetch");
-    if (evt.request.url.includes("/api/")) {
-        evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                    .then(response => {
-                        // If the response was good, clone it and store it in the cache.
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
+            console.log("fetch");
+            if (evt.request.url.includes("/api/")) {
+                evt.respondWith(
+                    caches.open(DATA_CACHE_NAME).then(cache => {
+                        return fetch(evt.request)
+                            .then(response => {
+                                // If the response was good, clone it and store it in the cache.
+                                if (response.status === 200) {
+                                    cache.put(evt.request.url, response.clone());
+                                }
+
+                                return response;
+                            })
+                            .catch(err => {
+                                // Network request failed, try to get it from the cache.
+                                return cache.match(evt.request);
+                            });
+                    }).catch(err => console.log(err))
+                );
+
+                return;
+            }
+
+            evt.respondWith(
+                fetch(evt.request).catch(function () {
+                    return caches.match(evt.request).then(function (response) {
+                        if (response) {
+                            return response;
+                        } else if (evt.request.headers.get("accept").includes("text/html")) {
+                            // return the cached home page for all requests for html pages
+                            return caches.match("/");
                         }
-
-                        return response;
-                    })
-                    .catch(err => {
-                        // Network request failed, try to get it from the cache.
-                        return cache.match(evt.request);
                     });
-            }).catch(err => console.log(err))
-        );
-
-        return;
-    }
-
-    evt.respondWith(
-        
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(evt.request).then(response => {
-                return response || fetch(evt.request);
+                })
             });
-        })
-    );
-});
